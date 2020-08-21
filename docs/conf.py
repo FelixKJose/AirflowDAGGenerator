c972b9276d93175edd2a65a845d225427cc15565
Felix Jose <felix.jose@philips.com>
2020-08-20 21:32:33 -0400

    Updated the Release version on conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Felix K Jose'
 author = 'Felix K Jose'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 
